2fecf55193b6ef5b7871b80d754863d716a6d0c8
Guilavogui <sayonguilavogui518@yahoo.com>
2021-01-24 02:28:30 +0800

    first commit

---
+++ AlgorithmAndDataStructure_In_Python/LinkedList/App.py
@@ -0,0 +1,14 @@
+
+
+from LinkedList import LinkedList
+
+linkedList = LinkedList()
+linkedList.insertEnd(12)
+linkedList.insertEnd(122)
+linkedList.insertEnd(34)
+linkedList.insertEnd(54)
+linkedList.insertEnd(62)
+
+linkedList.traverseList()
+
+linkedList.remove(12)
